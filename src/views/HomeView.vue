7bef9ac070d9fad0c43eceacebb9f798c5fb10cf
chrisais9 <chrisais9@naver.com>
2022-02-21 22:11:58 +0900

    :rotating_light: eslint+prettier 조합으로 프로젝트 재구성

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Playground NFT 세계에 오신것을 환영합니다." />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     HelloWorld,
   },
