8f11780a36c194313163ed98bbb8e95e680e50c2
chrisais9 <chrisais9@naver.com>
2022-02-22 13:52:19 +0900

    :lipstick: GNB 백그라운드 색상 및 컨텐츠 위치, 색상 변경

--- src/components/GNB.vue
+++ src/components/GNB.vue
@@ -1,16 +1,14 @@
 <template>
-  <div id="the-gnb">
-    <div class="the-gnb__left">
-      <div class="the-gnb__logo" @click="$router.push('/')">
-        <img class="desktop" src="@/assets/logo.png" alt="" />
-      </div>
+  <div id="gnb">
+    <div class="gnb__content">
+      <img class="desktop" src="@/assets/logo.png" alt="" />
+      <nav>
+        <router-link class="router-item" to="/">Home</router-link>
+        <router-link class="router-item" to="/about">About</router-link>
+        <router-link class="router-item" to="/claim">Claim</router-link>
+        <router-link class="router-item" to="/vote">Vote</router-link>
+      </nav>
     </div>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/claim">Claim</router-link>
-      <router-link to="/vote">Vote</router-link>
-    </nav>
   </div>
 </template>
 
@@ -22,18 +20,39 @@ export default class GNB extends Vue {}
 </script>
 
 <style lang="scss">
-#the-gnb {
+#gnb {
   display: flex;
   justify-content: space-between;
   align-items: center;
 
-  padding: 28px 60px;
+  padding: 28px 28px;
   height: 100px;
 
-  border-bottom: 1px solid #d2d5db;
-}
+  border-bottom: 1px solid #ff00e5;
+  background: #040303;
 
-nav {
-  padding: 1em;
+  .gnb__content {
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+
+    padding: 30px;
+
+    .router-item {
+      color: #ff00e5;
+
+      padding: 1em;
+
+      font-size: 1em;
+      font-weight: bold;
+      white-space: nowrap;
+      text-align: center;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 }
 </style>
