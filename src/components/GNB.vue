e5abc4a55db9ee61dd918d069ba000722ce86732
chrisais9 <chrisais9@naver.com>
2022-02-22 17:02:51 +0900

    :bento: 네비게이션 내에 각 소셜 로고들 추가 (오픈씨, 트위터 등)

--- src/components/GNB.vue
+++ src/components/GNB.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="gnb">
-    <div class="gnb__content">
+    <div class="gnb__left">
       <img class="desktop" src="@/assets/logo.png" alt="" />
       <nav>
         <router-link class="router-item" to="/">Home</router-link>
@@ -8,6 +8,20 @@
         <router-link class="router-item" to="/claim">Claim</router-link>
         <router-link class="router-item" to="/vote">Vote</router-link>
       </nav>
+    </div>
+    <div class="gnb__right">
+      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <img class="social-icon" src="@/assets/icon/facebook.png" />
+      </a>
+      <a href="https://opensea.io" target="_blank" rel="noopener noreferrer">
+        <img class="social-icon" src="@/assets/icon/opensea.svg" />
+      </a>
+      <a href="https://discord.com/branding" target="_blank" rel="noopener noreferrer">
+        <img class="social-icon" src="@/assets/icon/discord.png" />
+      </a>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <img class="social-icon" src="@/assets/icon/twitter.png" />
+      </a>
     </div>
   </div>
   <div class="gnb__secondary-border"></div>
@@ -33,15 +47,12 @@ export default class GNB extends Vue {}
   border-bottom: 1px solid $primary-color;
   background: #040303;
 
-  .gnb__content {
-    cursor: pointer;
-
+  .gnb__left {
     display: flex;
     align-items: center;
 
-    padding: 30px;
-
     .router-item {
+      cursor: pointer;
       text-decoration: none;
       color: $primary-color;
       padding: 1em;
@@ -54,6 +65,17 @@ export default class GNB extends Vue {}
       &.router-link-exact-active {
         color: #42b983;
       }
+    }
+  }
+
+  .gnb__right {
+    display: flex;
+    align-items: center;
+
+    .social-icon {
+      width: 30px;
+      height: 30px;
+      margin-left: 1em;
     }
   }
 }
