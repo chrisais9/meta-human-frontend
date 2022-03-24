b94afe514876a77569779b64d818490ea4ce9733
chrisais9 <chrisais9@naver.com>
2022-03-24 15:45:49 +0900

    :building_construction: 랜딩페이지 View단으로 분리

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,21 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <v-row no-gutters>
-          <v-col class="offset-lg-4 mb-n8" lg="4" style="z-index: 1">
-            <v-img width="300px" src="https://ik.imagekit.io/bayc/assets/bayc-logo.png" />
-          </v-col>
-
-          <v-col class="justify-end d-none d-lg-flex" lg="4">
-            <v-icon class="px-4">mdi-youtube</v-icon>
-            <v-icon class="px-4">mdi-instagram</v-icon>
-            <v-icon class="px-4">mdi-discord</v-icon>
-            <v-icon class="px-4">mdi-twitter</v-icon>
-          </v-col>
-
-          <v-col class="px-0" cols="12">
-            <v-container class="mb-4 mb-lg-5"
-              ><video autoplay loop id="myVideo">
-                <source
-                  src="https://ik.imagekit.io/bayc/assets/club-landing.mp4"
-                  type="video/mp4"
-                /></video
-            ></v-container>
-          </v-col>
-
-          <v-col lg="4">
-            <v-row class="pt-lg-4">
-              <v-col>
-                <div class="text-h5">WELCOME TO THE BORED APE YACHT CLUB</div>
-                <v-btn color="green" class="mb-4" width="100%">ENTER</v-btn>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-        <v-row> </v-row>
-      </v-container>
+      <LandingView />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-import { NFTContractModule } from "./store/web3/NFTContractModule";
+import LandingView from "@/views/LandingView.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    LandingView,
   },
 })
-export default class App extends Vue {
-  get alignments() {
-    return ["start", "center", "end"];
-  }
-  async mounted() {
-    await NFTContractModule.establishContract();
-  }
-}
+export default class App extends Vue {}
 </script>
 
 <style lang="scss" scoped>
