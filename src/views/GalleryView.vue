5a6526363f03982c427f6f40fbd56297fcf9e1e7
chrisais9 <chrisais9@naver.com>
2022-03-28 14:19:36 +0900

    :lipstick: 갤러리내 기본 UI 레이아웃 구성

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,6 +1,23 @@
 <template>
   <v-container fluid>
-    <h1>This is gallery</h1>
+    <v-row>
+      <v-form>
+        <div class="text-h4 font-weight-black">FILTER</div>
+        <v-switch v-model="isGoldenMode" label="Golden Mode"></v-switch>
+        <v-text-field append-icon="mdi-magnify" @click:append="appendIconCallback">
+          Default Slot
+        </v-text-field>
+      </v-form>
+      <v-col>
+        <v-row>
+          <v-col cols="3" v-for="n in 12" :key="n">
+            <v-card>
+              <v-card-header-text>Test</v-card-header-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -8,7 +25,9 @@
 import { Options, Vue } from "vue-class-component";
 
 @Options({})
-export default class GalleryView extends Vue {}
+export default class GalleryView extends Vue {
+  isGoldenMode = true;
+}
 </script>
 
 <style lang="scss" scoped></style>
