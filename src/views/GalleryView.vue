c5f67d2d217a1734829af235c6fc2671ccace326
chrisais9 <chrisais9@naver.com>
2022-04-01 11:45:30 +0900

    :recycle: NFTDetailDialog로 컴포넌트화

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -79,81 +79,23 @@
       </v-col>
     </v-row>
 
-    <v-dialog v-model="isDetailDialogShown">
-      <v-card class="overflow-auto" style="height: 500px">
-        <v-row>
-          <v-col cols="12">
-            <v-img width="300" :src="image(clickedDetailId)" />
-          </v-col>
-          <v-col cols="12">
-            <div class="text-caption">L.L.HB</div>
-          </v-col>
-          <v-col>
-            <div cols="6" class="d-flex justify-space-between">
-              <div class="text-h4">#3122</div>
-            </div>
-          </v-col>
-          <v-col cols="6">
-            <v-btn
-              href="https://testnets.opensea.io/assets/0x6e93f075d41dbb74c37dba9ba19a0b33c6545d4d/12"
-              target="_black"
-            >
-              오픈씨
-            </v-btn>
-          </v-col>
-
-          <v-col cols="4">
-            <v-sheet class="ma-4 pa-2" color="grey">
-              <v-row>
-                <v-col>
-                  <v-icon>mdi-human</v-icon>
-                </v-col>
-                <v-col>
-                  <div class="text-caption">TYPE:</div>
-                  <div class="text-body">GHOST</div>
-                </v-col>
-              </v-row>
-            </v-sheet>
-          </v-col>
-          <v-col cols="4">
-            <v-sheet class="ma-4 pa-2" color="grey">
-              <v-row>
-                <v-col>
-                  <v-icon cla>mdi-human</v-icon>
-                </v-col>
-                <v-col>
-                  <div class="text-caption">TYPE:</div>
-                  <div class="text-body">GHOST</div>
-                </v-col>
-              </v-row>
-            </v-sheet>
-          </v-col>
-          <v-col cols="4">
-            <v-sheet class="ma-4 pa-2" color="grey">
-              <v-row>
-                <v-col>
-                  <v-icon cla>mdi-human</v-icon>
-                </v-col>
-                <v-col>
-                  <div class="text-caption">TYPE:</div>
-                  <div class="text-body">GHOST</div>
-                </v-col>
-              </v-row>
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-card>
+    <v-dialog v-model="isShowingDetailDialog">
+      <NFTDetailDialog :id="clickedDetailId" />
     </v-dialog>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-
-@Options({})
+import NFTDetailDialog from "@/components/NFTDetailDialog.vue";
+@Options({
+  components: {
+    NFTDetailDialog,
+  },
+})
 export default class GalleryView extends Vue {
   isGoldenMode = true;
-  isDetailDialogShown = false;
+  isShowingDetailDialog = false;
   clickedDetailId = 1;
   tab = "temp";
 
@@ -194,14 +136,13 @@ export default class GalleryView extends Vue {
       },
     ];
   }
-
   image(value: number) {
     return new URL(`../assets/hb/hb${value}.png`, import.meta.url).href;
   }
 
   showDetailDialog(id: number) {
     this.clickedDetailId = id;
-    this.isDetailDialogShown = true;
+    this.isShowingDetailDialog = true;
   }
 }
 </script>
