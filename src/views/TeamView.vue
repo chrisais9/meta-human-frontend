4ef0b602ed31da4b68f9e3ed73e3b8d799495c4f
chrisais9 <chrisais9@naver.com>
2022-04-05 17:01:04 +0900

    :clown_face: 임시로 팀소개 페이지에 nft carousel 추가

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,6 +1,15 @@
 <template>
-  <v-container fluid>
+  <v-container>
     <h1>This is The Team</h1>
+    <v-carousel cycle>
+      <v-carousel-item v-for="i in 4" :key="i">
+        <v-sheet height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <v-img :src="image(i)"></v-img>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </v-container>
 </template>
 
@@ -8,7 +17,11 @@
 import { Options, Vue } from "vue-class-component";
 
 @Options({})
-export default class TeamView extends Vue {}
+export default class TeamView extends Vue {
+  image(value: number) {
+    return new URL(`../assets/hb/hb${value}.png`, import.meta.url).href;
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
