c0e7210edf756b779b99172683c4e9567a8cce14
chrisais9 <chrisais9@naver.com>
2022-04-13 15:52:39 +0900

    :sparkles: bootstrap 추가 / Navbar 추가 / eslint,prettier로 린트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { Link } from "react-router-dom";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>Bookkeeper</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
-      </nav>
-    </div>
+    <>
+      <Navbar bg="transparent">
+        <Container>
+          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+          <Nav className="mx-auto">
+            <Nav.Link href="/expenses">Expenses</Nav.Link>
+            <Nav.Link href="/invoices">Invoices</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Expenses />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-const Caver = require('caver-js')
-const caver = new Caver('https://api.baobab.klaytn.net:8651/')
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Caver = require("caver-js");
+const caver = new Caver("https://api.baobab.klaytn.net:8651/");
 
 async function testFunction() {
-	const version = await caver.rpc.klay.getClientVersion()
-	console.log(version)
+  const version = await caver.rpc.klay.getClientVersion();
+  console.log(version);
 }
 
-testFunction()
+testFunction();
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
