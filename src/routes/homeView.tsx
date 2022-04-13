be6babf20122c45509231dc1a9a729b4dbff0183
chrisais9 <chrisais9@naver.com>
2022-04-13 18:09:33 +0900

    :heavy_plus_sign: styled-component 라이브러리 추가 / 홈 화면에 비디오 백그라운드 추가

--- src/routes/homeView.tsx
+++ src/routes/homeView.tsx
@@ -3,7 +3,15 @@ import React from "react";
 export default function HomeView() {
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Home</h2>
+      <video
+        className="d-block"
+        autoPlay
+        muted
+        loop
+        style={{ height: "100vh", width: "100%", marginTop: "-90px", objectFit: "cover" }}
+      >
+        <source src="https://azk.imgix.net/beanzwuzhere.mp4" type="video/mp4" />
+      </video>
     </main>
   );
 }
