3076f3dbde9785c11343dd9e58ea69b0b478e2b1
chrisais9 <chrisais9@naver.com>
2022-04-18 18:43:38 +0900

    :lipstick: 비디오 자동재생이 지원되지 않는 브라우저는 홈화면에 정적인 이미지 보여줌

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import MainLayout from "../components/NavBar/MainLayout";
 
 const Home: NextPage = () => {
@@ -13,6 +14,12 @@ const Home: NextPage = () => {
       >
         <source src="/assets/video/beanzwuzhere.mp4" />
       </video>
+      <Image
+        className="min-w-screen w-full lg:block bottom-0 transform h-screen object-cover absolute top-0 left-0 object-center overlay-item -z-10"
+        src="/assets/images/beanzwashere.jpg"
+        layout="fill"
+        alt="background"
+      ></Image>
     </MainLayout>
   );
 };
