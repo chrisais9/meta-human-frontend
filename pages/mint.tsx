bd463df5887a27b670c9fe5c31be303ce8f78aef
chrisais9 <chrisais9@naver.com>
2022-04-18 15:12:35 +0900

    :sparkles: 민팅하는 로직 정상 작동 확인 / 민팅 되면 alert 하고 윈도우 재로딩

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,45 +1,79 @@
+import Caver, { AbiItem } from "caver-js";
 import type { NextPage } from "next";
 import { useEffect, useRef, useState } from "react";
 import MainLayout from "../components/NavBar/MainLayout";
-import useInterval from "../hooks/useInterval";
-import caver from "../service/klaytn/caver";
+import ABI from "../abi/abi.json";
 
 const Mint: NextPage = () => {
-  const [version, setVersion] = useState("");
-  const [currectBlockNumber, setCurrentBlockNumber] = useState(0);
-  const [wallectAddress, setWalletAddress] = useState("");
+  const [walletAddress, setWalletAddress] = useState("");
 
-  let getBlockNumber = async () => {
-    const blockNumber = await caver.klay.getBlockNumber();
-    setCurrentBlockNumber(blockNumber);
-  };
+  const [collectionName, setCollectionName] = useState("");
+  const [totalSupply, setTotalSupply] = useState(0);
+
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+    fetchCollectionName();
+    fetchTotalSupply();
+
+    return () => {
+      console.log("컴포넌트가 화면에서 사라짐");
+    };
+  }, []);
+
+  async function fetchCollectionName() {
+    const caver = new Caver(window.klaytn);
+    const contract = new caver.klay.Contract(
+      ABI as AbiItem[],
+      "0xa4e0931470700187317B551B1c06733Df6645758"
+    );
+    const collectionName = await contract.methods.name().call();
+
+    setCollectionName(collectionName);
+  }
+
+  async function fetchTotalSupply() {
+    const caver = new Caver(window.klaytn);
+    const contract = new caver.klay.Contract(
+      ABI as AbiItem[],
+      "0xa4e0931470700187317B551B1c06733Df6645758"
+    );
+    const totalSupply = await contract.methods.totalSupply().call();
+
+    setTotalSupply(totalSupply);
+  }
 
   async function connectWallet() {
     const accounts = await window.klaytn.enable();
     setWalletAddress(accounts[0]);
   }
 
-  function mint() {}
+  async function mint() {
+    const caver = new Caver(window.klaytn);
+    const contract = new caver.klay.Contract(
+      ABI as AbiItem[],
+      "0xa4e0931470700187317B551B1c06733Df6645758"
+    );
 
-  useInterval(() => {
-    getBlockNumber();
-  }, 1000);
+    const receipt = await contract.methods.mintHoneyBadger("1").send({
+      from: walletAddress,
+      value: caver.utils.toPeb("0.1", "KLAY"),
+      gas: 1000000,
+    });
+    console.log(receipt);
+    if (!receipt.txError) {
+      alert("민팅 성공");
+      window.location.reload();
+    }
+  }
 
-  useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
-    setVersion(caver.version);
-
-    return () => {
-      console.log("컴포넌트가 화면에서 사라짐");
-    };
-  }, []);
   return (
     <MainLayout>
       <div className="container mx-auto mt-32">
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-          <div>클레이튼 block number: {currectBlockNumber}</div>
-          <div>클레이튼 version: {version}</div>
-          <div>카이카스 지갑주소: {wallectAddress}</div>
+          <div>카이카스 지갑주소: {walletAddress}</div>
+          <hr className="my-6" />
+          <div>콜렉션이름: {collectionName}</div>
+          <div>민트된 NFT 수: {totalSupply}</div>
           <hr className="my-6" />
           <div className="mb-6">
             <label className="block text-sm font-bold mb-2">민팅할 수량</label>
@@ -52,7 +86,7 @@ const Mint: NextPage = () => {
             <p className="text-xs italic">최대 5개까지 민팅 가능합니다.</p>
           </div>
           <div className="flex items-center justify-center">
-            {wallectAddress.length == 0 ? (
+            {walletAddress.length == 0 ? (
               <button
                 className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
                 type="button"
