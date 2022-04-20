71048db12d8332b5d8d1528088275414ad881b9b
chrisais9 <chrisais9@naver.com>
2022-04-20 16:33:55 +0900

    :lipstick: 상단 헤더 디자인 명세에 맞도록 수정 / toggle switch 라이브러리 추가

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import Switch from "react-switch";
 
 const routerItems = [
   {
@@ -8,7 +11,7 @@ const routerItems = [
   },
   {
     href: "/mint",
-    title: "Buy(Mint)",
+    title: "Buy",
   },
   {
     href: "/gallery",
@@ -18,27 +21,95 @@ const routerItems = [
     href: "/team",
     title: "Team",
   },
+  {
+    href: "/shop",
+    title: "Shop",
+  },
+];
+
+const socialItems = [
+  {
+    href: "https://www.twitter.com",
+    title: "twitter",
+  },
+  {
+    href: "https://www.instagram.com",
+    title: "instagram",
+  },
+  {
+    href: "https://www.discord.com",
+    title: "discord",
+  },
+  {
+    href: "https://opensea.io",
+    title: "opensea",
+  },
 ];
 
 export default function NavBar() {
   const router = useRouter();
+
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
   return (
-    <nav className="fixed top-0 z-50 w-full px-5 pt-5 lg:px-8 lg:pt-8">
-      <div className="max-w-11xl mx-auto flex h-full items-center justify-center border-b border-white border-opacity-0">
+    <nav className="fixed top-0 z-50 h-20 w-full px-6 pt-7">
+      <div className="mx-auto flex h-full items-center justify-center">
         <Link href="/">
-          <span className="flex-grow text-2xl font-bold">META-HUMAN</span>
+          <span className="flex-grow cursor-pointer font-mono text-2xl font-bold">
+            META HUMAN
+          </span>
         </Link>
         <div className="hidden items-center lg:flex" id="mobile-menu">
-          <ul className="flex space-x-2">
+          <ul className="flex space-x-6">
             {routerItems.map(({ href, title }) => (
-              <li key={title}>
+              <li
+                key={title}
+                className={`rounded p-2 text-2xs ${
+                  router.pathname == href
+                    ? "bg-black text-header-btn-background"
+                    : "bg-header-btn-background text-black"
+                }`}
+              >
                 <Link href={href}>
-                  <a className="text-2xs font-500 h-7 items-center rounded bg-white bg-opacity-20 py-1 px-4 font-mono uppercase tracking-wider text-black opacity-80 duration-200 hover:bg-opacity-70 ">
-                    {title}
-                  </a>
+                  <a>{title}</a>
                 </Link>
               </li>
             ))}
+            {socialItems.map(({ href, title }) => (
+              <li
+                key={title}
+                className="rounded bg-header-btn-background p-2 text-2xs"
+              >
+                <a target="_blank" href={href} rel="noopener noreferrer">
+                  <Image
+                    src={`/assets/icons/${title}.svg`}
+                    width="12px"
+                    height="12px"
+                    alt={title}
+                  />
+                </a>
+              </li>
+            ))}
+            <li>
+              <Switch
+                width={150}
+                onChange={setIsWalletConnected}
+                checked={isWalletConnected}
+                offColor="#F5F6F8"
+                onColor="#000000"
+                offHandleColor="#000000"
+                uncheckedIcon={
+                  <div className="flex h-full items-center justify-end text-base">
+                    Connect&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  </div>
+                }
+                checkedIcon={
+                  <div className="flex h-full items-center justify-start text-base text-white">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Connected
+                  </div>
+                }
+              />
+            </li>
           </ul>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "header-btn-background": "#F5F6F8",
+        black: "#000000",
+      },
+      fontSize: {
+        "2xs": ".5rem",
+      },
+    },
   },
   plugins: [],
 };
