7ea7ff14eb8aeb6371b1ec11d775fdbae79272d6
chrisais9 <chrisais9@naver.com>
2022-05-11 13:30:14 +0900

    :zap: NFT fetch 로직 개선

--- src/lib/fetchNFTs.ts
+++ src/lib/fetchNFTs.ts
@@ -1,38 +1,40 @@
 import caver from "@/config/caver";
 import INFT from "@/schema/INFT";
-import { AbiItem } from "caver-js";
+import { AbiItem, Contract } from "caver-js";
 import ABI from "@/abi/abi.json";
 import axios from "axios";
 
 const contractAddress = "0xa4e0931470700187317B551B1c06733Df6645758";
 
-export async function fetchNFTs(): Promise<INFT[]> {
+async function fetchNFT(contract: Contract, id: number): Promise<INFT> {
+  const hash = await contract.methods.tokenURI(id).call();
+  const owner = await contract.methods.ownerOf(id).call();
+
+  const response = await axios.get(hash);
+  if (response.status === 200) {
+    const metadata = await response.data;
+    const imageCID = (metadata.image as string).split("//")[1];
+    return {
+      id: id,
+      name: metadata.name,
+      color: "todo",
+      image: `https://ipfs.io/ipfs/${imageCID}`,
+      attributes: [],
+    };
+  } else {
+    throw new Error("Something went wrong");
+  }
+}
+
+export async function fetchCollection(): Promise<INFT[]> {
   const contract = new caver.klay.Contract(ABI as AbiItem[], contractAddress);
   const totalSupply = await contract.methods.totalSupply().call();
-  let collection: INFT[] = [];
-  for (let i = 1; i <= totalSupply; i++) {
-    const hash = await contract.methods.tokenURI(i).call();
-    const owner = await contract.methods.ownerOf(i).call();
-    try {
-      const response = await axios.get(hash);
-      if (response.status != 200) {
-        throw new Error("Something went wrong");
-      }
 
-      const metadata = await response.data;
-      const image = (metadata.image as string).split("//")[1]; // ipfs://something -> something
-      // collection = [
-      //   ...collection,
-      //   {
-      //     id: i,
-      //     name: metadata.name,
-      //     image: `https://ipfs.io/ipfs/${image}`,
-      //   },
-      // ];
-    } catch (e) {
-      console.error("Something went wrong", e);
-    }
-  }
+  const collection = await Promise.all(
+    Array(totalSupply)
+      .fill(0)
+      .map((e, i) => fetchNFT(contract, i + 1)) // [fetchNFT(1), fetchNFT(2) ... fetchNFT(9999), fetchNFT(10000)]
+  );
 
   return collection;
 }
